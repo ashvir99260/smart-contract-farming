059d74d8786238758e31c7ca3dacb8b3f834587f
Ashish Virde <ashvir99260@gmail.com>
2022-12-02 22:13:44 +0530

    Added basic metamask login functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+import useMetaMask from "./context/MetaMaskContext";
 
 function App() {
+  const { connect, disconnect, isActive } = useMetaMask();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button onClick={connect} variant="secondary">
+          <img
+            src="images/metamask.svg"
+            alt="MetaMask"
+            width="50"
+            height="50"
+          />{" "}
+          Login
+        </Button>
+        {isActive && (
+          <Button onClick={disconnect} variant="danger">
+            Disconnect from MetaMask{" "}
+            <img src="images/noun_waving_3666509.svg" height="50" width="50" />
+          </Button>
+        )}
       </header>
     </div>
   );
---
+++ src/context/MetaMaskContext.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useMemo, useCallback } from "react";
+import { injected } from "./wallet/injected";
+import { useWeb3React } from "@web3-react/core";
+
+export const MetaMaskContext = React.createContext(null);
+
+export const MetaMaskProvider = ({ children }) => {
+  const { activate, account, active, deactivate } = useWeb3React();
+
+  const [isActive, setIsActive] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Init Loading
+  useEffect(() => {
+    connect().then((val) => {
+      setIsLoading(false);
+    });
+  }, []);
+
+  const handleIsActive = useCallback(() => {
+    setIsActive(active);
+  }, [active]);
+
+  useEffect(() => {
+    handleIsActive();
+  }, [handleIsActive]);
+
+  // Connect to MetaMask wallet
+  const connect = async () => {
+    try {
+      await activate(injected);
+    } catch (error) {
+      console.log("Error on connecting: ", error);
+    }
+  };
+
+  // Disconnect from Metamask wallet
+  const disconnect = async () => {
+    try {
+      await deactivate();
+    } catch (error) {
+      console.log("Error on disconnecting: ", error);
+    }
+  };
+
+  const values = useMemo(
+    () => ({
+      isActive,
+      account,
+      isLoading,
+      connect,
+      disconnect,
+    }),
+    [isActive, isLoading]
+  );
+
+  return (
+    <MetaMaskContext.Provider value={values}>
+      {children}
+    </MetaMaskContext.Provider>
+  );
+};
+
+export default function useMetaMask() {
+  const context = React.useContext(MetaMaskContext);
+
+  if (context === undefined) {
+    throw new Error(
+      "useMetaMask hook must be used with a MetaMaskProvider component"
+    );
+  }
+
+  return context;
+}
