5dfbf45d4b8331c09b4aac95f1a5bd96b968121a
Amit Negi <amit.negi077@gmail.com>
2022-12-04 01:42:22 +0530

    changed contract addressess

--- src/web3/constants.js
+++ src/web3/constants.js
@@ -1,4 +1,4 @@
 export const farmerContractAddress =
-  "0x1F57C6462f76a2997D664919e7D4871bbe852dD6";
+  "0x1accfC75b8098ac79833882D0BC78752c7d5ED64";
 export const ICSBuyerContractAddress =
-  "0x91A740c65F554c6A7A509e1a99e3F7fAe4fE88EB";
+  "0x54a57afe1726479de82B6d7C0C72deD83496fda1";
